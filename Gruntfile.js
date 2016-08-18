70948605a220a7a49a5f387b8aa9e72c6b8f3a76
Pierre Rudloff <contact@rudloff.pro>
2016-08-18 14:06:06 +0200

    Use local phpunit and phpcs Lint

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,11 +6,36 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-shipit');
     grunt.loadNpmTasks('shipit-git-update');
     grunt.loadNpmTasks('shipit-composer-simple');
+    grunt.loadNpmTasks('grunt-phpcs');
+    grunt.loadNpmTasks('grunt-phpunit');
 
     grunt.initConfig({
         jslint: {
             Gruntfile: {
                 src: 'Gruntfile.js'
+            }
+        },
+        phpcs: {
+            options: {
+                standard: 'PSR2',
+                bin: 'vendor/bin/phpcs'
+            },
+            php: {
+                src: ['*.php', 'classes/*.php']
+            },
+            tests: {
+                src: ['tests/*.php']
+            }
+        },
+        phpunit: {
+            options: {
+                bin: 'php -dzend_extension=xdebug.so ./vendor/bin/phpunit',
+                stopOnError: true,
+                stopOnFailure: true,
+                followOutput: true
+            },
+            classes: {
+                dir: 'tests/'
             }
         },
         shipit: {
@@ -25,7 +50,8 @@ module.exports = function (grunt) {
         }
     });
 
-    grunt.registerTask('lint', ['jslint']);
+    grunt.registerTask('lint', ['phpcs', 'jslint']);
+    grunt.registerTask('test', ['phpunit']);
     grunt.registerTask('prod', ['shipit:prod', 'update', 'composer:install']);
     grunt.registerTask('satis', ['shipit:prod', 'composer:cmd']);
 };
